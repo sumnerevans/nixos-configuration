637c5354b0122174998a42142dee52f1eeb5ea76
Sumner Evans <me@sumnerevans.com>
2020-09-28 10:39:39 -0600

    Fixes #10: use redshift-gtk instead of redshift

--- services/redshift.nix
+++ services/redshift.nix
@@ -1,53 +1,30 @@
-{ config, pkgs, lib, ... }:
+{ lib, ... }:
 {
-  # TODO switch to redshift-gtk
-  location.provider = "geoclue2";
-  services.redshift = {
-    enable = true;
+  config = {
+    location.provider = "geoclue2";
+    services.redshift = {
+      enable = true;
 
-    brightness = {
-      day = "1";
-      night = "0.9";
+      brightness = {
+        day = "1";
+        night = "0.9";
+      };
+
+      temperature = {
+        day = 5500;
+        night = 4000;
+      };
     };
 
-    temperature = {
-      day = 5500;
-      night = 4000;
+    systemd.user.services.redshift = {...}: {
+      options = {
+        serviceConfig = lib.mkOption {
+          apply = opts: opts // {
+            ExecStart = builtins.replaceStrings [ "/bin/redshift" ] [ "/bin/redshift-gtk" ] opts.ExecStart;
+          };
+        };
+      };
+      config = {};
     };
   };
-
-  # Use redshift-gtk instead of redshift.
-  # nixpkgs.overlays = [
-  #   (self: super: {
-  #     picom = super.systemd.user.services.overrideAttrs (old: let
-  #       cfg = old.cfg;
-  #       lcfg = old.lcfg;
-  #       providerString = old.providerString;
-  #     in {
-  #       ExecStart = ''
-  #         ${cfg.package}/bin/redshift-gtk \
-  #           -l ${providerString} \
-  #           -t ${toString cfg.temperature.day}:${toString cfg.temperature.night} \
-  #           -b ${toString cfg.brightness.day}:${toString cfg.brightness.night} \
-  #           ${lib.strings.concatStringsSep " " cfg.extraOptions}
-  #       '';
-  #     });
-  #   })
-  # ];
-
-  # systemd.user.services.redshift.serviceConfig = let
-  #   cfg = config.services.redshift;
-  #   lcfg = config.location;
-  #   providerString = if lcfg.provider == "manual"
-  #     then "${toString lcfg.latitude}:${toString lcfg.longitude}"
-  #     else lcfg.provider;
-  # in {
-  #   ExecStart = ''
-  #     ${cfg.package}/bin/redshift-gtk \
-  #       -l ${providerString} \
-  #       -t ${toString cfg.temperature.day}:${toString cfg.temperature.night} \
-  #       -b ${toString cfg.brightness.day}:${toString cfg.brightness.night} \
-  #       ${lib.strings.concatStringsSep " " cfg.extraOptions}
-  #   '';
-  # };
 }
