008bfb2e0227c0a4ed2b2420056ab54aced5133e
Sumner Evans <me@sumnerevans.com>
2021-01-04 23:37:39 -0700

    Add back chromeos enablement stack

--- hardware-configurations/mustafar/default.nix
+++ hardware-configurations/mustafar/default.nix
@@ -7,6 +7,38 @@ in
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
   boot.blacklistedKernelModules = [ "snd_hda_intel" "snd_soc_skl" ];
+  boot.kernelPatches = [
+    {
+      name = "chromebook-config";
+      patch = null;
+      extraConfig = ''
+        CHROMEOS_LAPTOP m
+        CHROMEOS_PSTORE m
+        CHROME_PLATFORMS y
+        CROS_EC m
+        CROS_EC_CHARDEV m
+        CROS_EC_DEBUGFS m
+        CROS_EC_I2C m
+        CROS_EC_LPC m
+        CROS_EC_SENSORHUB m
+        CROS_EC_SPI m
+        CROS_EC_SYSFS m
+        CROS_EC_TYPEC m
+        CROS_USBPD_LOGGER m
+        EXTCON_USBC_CROS_EC m
+        I2C_CROS_EC_TUNNEL m
+        IIO_CROS_EC_ACCEL_LEGACY m
+        IIO_CROS_EC_BARO m
+        IIO_CROS_EC_LIGHT_PROX m
+        IIO_CROS_EC_SENSORS m
+        IIO_CROS_EC_SENSORS_CORE m
+        IIO_CROS_EC_SENSORS_LID_ANGLE m
+        KEYBOARD_CROS_EC m
+        RTC_DRV_CROS_EC m
+        SND_SOC_CROS_EC_CODEC m
+      '';
+    }
+  ];
 
   # Orientation and ambient light
   hardware.sensor.iio.enable = true;
