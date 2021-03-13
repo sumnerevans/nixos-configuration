96337381fccefb2f50d6a7fe1d5d95e2602c5c95
Sumner Evans <me@sumnerevans.com>
2021-03-12 21:35:20 -0700

    Updates for new coruscant

--- host-configurations/coruscant.nix
+++ host-configurations/coruscant.nix
@@ -3,5 +3,9 @@
   networking.hostName = "coruscant";
   hardware.ramSize = 32;
   xorg.enable = true;
-  xorg.xkbVariant = "3l";
+
+  networking.interfaces.enp37s0.useDHCP = true;
+  networking.interfaces.wlp35s0.useDHCP = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
