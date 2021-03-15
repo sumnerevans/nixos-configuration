364ccc418d8d1ebd98206e03fd96533521d7ddef
Sumner Evans <me@sumnerevans.com>
2021-03-15 00:00:55 -0600

    Move most of the i3 stuff to home manager

--- host-configurations/coruscant.nix
+++ host-configurations/coruscant.nix
@@ -3,7 +3,6 @@
   networking.hostName = "coruscant";
   hardware.ramSize = 32;
   xorg.enable = true;
-  xorg.remapEscToCaps = false;
 
   networking.interfaces.enp37s0.useDHCP = true;
   networking.interfaces.wlp35s0.useDHCP = true;
--- services/i3wm.nix
+++ services/i3wm.nix
@@ -15,34 +15,14 @@ in
         description = "The XKB variant to use";
         default = "";
       };
-
-      remapEscToCaps = mkOption {
-        type = types.bool;
-        description = "Remap the physical escape key to Caps Lock";
-        default = true;
-      };
     };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      dunst
-      flameshot
-      lxappearance
-      scrot
-      xbindkeys
-      xclip
-      xorg.xbacklight
-      xorg.xdpyinfo
-      xorg.xprop
-    ];
-
-    services.xbanish.enable = true;
-
     services.xserver = {
       # Enable the X11 windowing system.
       enable = true;
-      displayManager.startx.enable = true;
+      windowManager.i3.enable = true;
 
       # Use 3l
       layout = "us";
@@ -53,28 +33,6 @@ in
         enable = true;
         touchpad.tapping = false;
       };
-
-      # Enable i3
-      windowManager.i3 = {
-        enable = true;
-        package = pkgs.i3-gaps;
-      };
     };
-
-    systemd.user.services.xmodmap = mkIf (cfg.remapEscToCaps) (
-      let
-        xmodmapConfig = pkgs.writeText "Xmodmap.conf" ''
-          ! Reverse scrolling
-          ! pointer = 1 2 3 5 4 6 7 8 9 10 11 12
-          keycode 9 = Caps_Lock Caps_Lock Caps_Lock
-        '';
-      in
-        {
-          description = "Run xmodmap on startup.";
-          wantedBy = [ "graphical-session.target" ];
-          partOf = [ "graphical-session.target" ];
-          serviceConfig.ExecStart = "${pkgs.xorg.xmodmap}/bin/xmodmap ${xmodmapConfig}";
-        }
-    );
   };
 }
