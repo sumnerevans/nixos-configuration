9bd53c5e16739184a0f50693918558bf8f356cb6
Sumner Evans <me@sumnerevans.com>
2021-05-23 09:08:18 -0600

    heisenbridge: use upstream @ 1f8df49

--- pkgs/heisenbridge.nix
+++ pkgs/heisenbridge.nix
@@ -1,13 +1,14 @@
 { lib, fetchFromGitHub, python3Packages }:
+
 python3Packages.buildPythonPackage rec {
   pname = "heisenbridge";
-  version = "unstable-2021-05-20";
+  version = "unstable-2021-05-23";
 
   src = fetchFromGitHub {
-    owner = "sumnerevans";
+    owner = "hifi";
     repo = "heisenbridge";
-    rev = "573b7f61d689715de7ff1bfdebf04686531c1793";
-    sha256 = "sha256-lI7TylNIJ9yydRvEVgm7mvKeyOJltjuQHBdlyHKsxrg=";
+    rev = "1f8df49b7e89aaeb2cbb5fee68bd29cf3eda079a";
+    sha256 = "sha256-ta6n9hXRdIjfxsLy9jrzZkz6TS50/TYpFOb/BLrRWK4=";
   };
 
   propagatedBuildInputs = with python3Packages; [
