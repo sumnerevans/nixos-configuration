dd1b6074487d988573ca8d0311a4459ae3c296a2
Sumner Evans <me@sumnerevans.com>
2021-07-07 13:07:51 -0600

    postgresql: perf tuning
    Used https://pgtune.leopard.in.ua/#/

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -1,28 +1,47 @@
 { config, lib, pkgs, ... }: with lib; mkMerge [
-  (mkIf config.services.postgresql.enable {
-    systemd.services.mkPostgresDataDir = {
-      description = "Make sure the postgres data directory exists before booting the service.";
-      wantedBy = [ "multi-user.target" ];
-      before = [ "postgresql.service" ];
-      serviceConfig = {
-        ExecStart = pkgs.writeShellScript "ensure-dirs" ''
-          mkdir -p ${config.services.postgresql.dataDir}
-          chown -R postgres:postgres ${config.services.postgresql.dataDir}
-        '';
+  (
+    mkIf config.services.postgresql.enable {
+      systemd.services.mkPostgresDataDir = {
+        description = "Make sure the postgres data directory exists before booting the service.";
+        wantedBy = [ "multi-user.target" ];
+        before = [ "postgresql.service" ];
+        serviceConfig = {
+          ExecStart = pkgs.writeShellScript "ensure-dirs" ''
+            mkdir -p ${config.services.postgresql.dataDir}
+            chown -R postgres:postgres ${config.services.postgresql.dataDir}
+          '';
+        };
       };
-    };
-  })
 
-  (mkIf config.services.postgresqlBackup.enable {
-    # Run backup every 3 hours.
-    services.postgresqlBackup = {
-      backupAll = true;
-      startAt = "0/3:0"; # systemd-analyze calendar "0/3:0"
-    };
+      services.postgresql.settings = {
+        max_connections = 20;
+        shared_buffers = "256MB";
+        effective_cache_size = "768MB";
+        maintenance_work_mem = "64MB";
+        checkpoint_completion_target = 0.9;
+        wal_buffers = "7864kB";
+        default_statistics_target = 100;
+        random_page_cost = 1.1;
+        effective_io_concurrency = 200;
+        work_mem = "6553kB";
+        min_wal_size = "1GB";
+        max_wal_size = "4GB";
+      };
+    }
+  )
 
-    # Add a backup service.
-    services.backup.backups.postgresql = {
-      path = config.services.postgresqlBackup.location;
-    };
-  })
+  (
+    mkIf config.services.postgresqlBackup.enable {
+      # Run backup every 3 hours.
+      services.postgresqlBackup = {
+        backupAll = true;
+        startAt = "0/3:0"; # systemd-analyze calendar "0/3:0"
+      };
+
+      # Add a backup service.
+      services.backup.backups.postgresql = {
+        path = config.services.postgresqlBackup.location;
+      };
+    }
+  )
 ]
