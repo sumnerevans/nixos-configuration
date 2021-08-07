a73bbafba655c8064f233e1b4d1d82760b669dd4
Sumner Evans <me@sumnerevans.com>
2021-08-07 12:01:15 -0600

    postgresql: increase max_connections to 500

--- modules/services/postgresql.nix
+++ modules/services/postgresql.nix
@@ -14,18 +14,22 @@
       };
 
       services.postgresql.settings = {
-        max_connections = 20;
-        shared_buffers = "256MB";
-        effective_cache_size = "768MB";
-        maintenance_work_mem = "64MB";
+        max_connections = 500;
+        shared_buffers = "512MB";
+        effective_cache_size = "1536MB";
+        maintenance_work_mem = "128MB";
         checkpoint_completion_target = 0.9;
-        wal_buffers = "7864kB";
+        wal_buffers = "16MB";
         default_statistics_target = 100;
         random_page_cost = 1.1;
         effective_io_concurrency = 200;
-        work_mem = "6553kB";
+        work_mem = "1048kB";
         min_wal_size = "1GB";
         max_wal_size = "4GB";
+        max_worker_processes = 2;
+        max_parallel_workers_per_gather = 1;
+        max_parallel_workers = 2;
+        max_parallel_maintenance_workers = 1;
       };
 
       systemd.services.postgresql = {
