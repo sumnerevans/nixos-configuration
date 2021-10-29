513befc47f2f771017f90bf3d9645feae7223b12
Sumner Evans <me@sumnerevans.com>
2021-10-29 08:49:20 -0600

    standupbot: 0.4.1rc3 -> 0.4.1rc4

--- pkgs/standupbot/default.nix
+++ pkgs/standupbot/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   pname = "standupbot";
-  version = "0.4.1rc3";
+  version = "0.4.1rc4";
 
   buildInputs = [ olm ];
 
@@ -10,8 +10,8 @@ buildGoPackage rec {
   src = fetchFromSourcehut {
     owner = "~sumner";
     repo = "standupbot";
-    rev = "ea60d9b796ee8f0e3b288160e725ffb31ff20ecd";
-    sha256 = "sha256-xrggcgncKNHKHySERLTrBE2QiycwmnPw3FVdPoyW/zA=";
+    rev = "8a49a24814806d3bf56120c5207abf5f48a18543";
+    sha256 = "sha256-+YWNQ+K5ATWP/zofNEG0aCMtbQTQWJu/vH29HfLP7xk=";
   };
 
   goDeps = ./deps.nix;
