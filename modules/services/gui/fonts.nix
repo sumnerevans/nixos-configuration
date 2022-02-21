c4a8c87c5acc9065e27ed6c06c4cddda907450ba
Sumner Evans <me@sumnerevans.com>
2022-02-20 18:54:48 -0700

    Revert "fonts: fix remarshal overlay for iosevka build"
    This reverts commit 9b60567e44bab8e23e584b5cd04bae3b71934760.

--- modules/services/gui/fonts.nix
+++ modules/services/gui/fonts.nix
@@ -1,18 +1,4 @@
 { config, lib, pkgs, ... }: {
-  nixpkgs.overlays = [
-    # https://github.com/NixOS/nixpkgs/pull/159074
-    (self: super: {
-      remarshal = super.remarshal.overrideAttrs (old: rec {
-        postPatch = ''
-          substituteInPlace pyproject.toml \
-            --replace "poetry.masonry.api" "poetry.core.masonry.api" \
-            --replace 'PyYAML = "^5.3"' 'PyYAML = "*"' \
-            --replace 'tomlkit = "^0.7"' 'tomlkit = "*"'
-        '';
-      });
-    })
-  ];
-
   fonts = lib.mkIf (config.xorg.enable || config.wayland.enable) {
     fonts = with pkgs; [
       font-awesome_4
