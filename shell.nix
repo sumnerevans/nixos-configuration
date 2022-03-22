d6386d27470bb97493e1d60f923fe962e5df64cb
Sumner Evans <me@sumnerevans.com>
2022-03-22 10:33:13 -0600

    pre-commit: add config

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   propagatedBuildInputs = with pkgs; [
+    cargo
     git-crypt
     gnutar
     nodePackages.bash-language-server
     openssl
     pass
+    pre-commit
     rnix-lsp
 
     (
