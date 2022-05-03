18f98cd613a816bc38c6a2be2a4fbf500a44dfde
Sumner Evans <me@sumnerevans.com>
2022-05-03 08:12:45 -0600

    matrix-synapse: 1.57.0 -> 1.58.0

--- modules/services/matrix/synapse/default.nix
+++ modules/services/matrix/synapse/default.nix
@@ -8,11 +8,11 @@ let
   package = pkgs.matrix-synapse.overridePythonAttrs (
     old: rec {
       pname = "matrix-synapse";
-      version = "1.57.0";
+      version = "1.58.0";
 
       src = pkgs.python3Packages.fetchPypi {
         inherit pname version;
-        sha256 = "sha256-pZhm3jfpqOcLT+M4eeD8FyHtwj5EOAFESFu+4ZMoz0s=";
+        sha256 = "sha256-cY3rtmaaAimEQPU4wcMEy/QysPNCdk7yptrkctnLfDA=";
       };
 
       doCheck = false;
