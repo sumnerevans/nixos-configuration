1f5fb245255b7c74d90c6beb0c66b61ce535fcce
Sumner Evans <me@sumnerevans.com>
2022-05-30 19:26:58 -0600

    mustafar: sound works

--- host-configurations/mustafar/default.nix
+++ host-configurations/mustafar/default.nix
@@ -9,41 +9,11 @@ in
   hardware.isLaptop = true;
   wayland.enable = true;
 
-  nixpkgs.overlays = [
-    # sof-firmware so sleep works on Kohaku
-    (
-      self: super: {
-        sof-firmware = super.sof-firmware.overrideAttrs (
-          old: rec {
-            version = "1.5.1";
-            src = super.fetchFromGitHub {
-              owner = "thesofproject";
-              repo = "sof-bin";
-              rev = "ae61d2778b0a0f47461a52da0d1f191f651e0763";
-              sha256 = "0j6bpwz49skvdvian46valjw4anwlrnkq703n0snkbngmq78prba";
-            };
-
-            installPhase = ''
-              mkdir -p $out/lib/firmware/intel
-              sed -i 's/ROOT=.*$/ROOT=$out/g' go.sh
-              sed -i 's/VERSION=.*$/VERSION=v${version}/g' go.sh
-              ./go.sh
-            '';
-          }
-        );
-      }
-    )
-  ];
-  # nix.enableRemoteBuildOnCoruscant = true;
-  # nix.enableRemoteBuildOnTatooine = true;
-
   virtualisation.docker.enable = true;
 
   # Get sound working
-  # hardware.firmware = [ sof-firmware ];
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
-  boot.blacklistedKernelModules = [ "snd_hda_intel" "snd_soc_skl" ];
 
   # Use systemd-boot
   boot.loader.systemd-boot.enable = true;
