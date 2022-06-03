2660af4976247094555ab15d444bb6ae1cce7a73
Sumner Evans <me@sumnerevans.com>
2022-06-02 20:49:44 -0600

    networking: add tailscale
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/hardware/networking.nix
+++ modules/hardware/networking.nix
@@ -4,6 +4,18 @@
     # here. Per-interface useDHCP will be mandatory in the future, so this
     # generated config replicates the default behaviour.
     networking.useDHCP = false;
+
+    # Enable tailscale across the fleet
+    services.tailscale.enable = true;
+    networking = {
+      firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+
+      # NixOS is kinda particular about how you configure the Magic DNS, so we have to do it a bit more manually. See: https://forum.tailscale.com/t/magicdns-nixos/412
+      nameservers = [ "100.100.100.100" ];
+      search = [ "sumnerevans.github.beta.tailscale.net" ];
+    };
+    environment.systemPackages = [ pkgs.tailscale ];
+
   }
 
   # If NetworkManager is enabled, then also enable strong swan integration.
