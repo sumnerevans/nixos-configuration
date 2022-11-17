b1f8864b434a28fff0116b4cc2a0bab37e634d59
Sumner Evans <me@sumnerevans.com>
2022-11-17 12:02:01 -0700

    maubot: fix dependencies
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- pkgs/maubot.nix
+++ pkgs/maubot.nix
@@ -9,24 +9,24 @@
 }:
 python3.pkgs.buildPythonPackage rec {
   pname = "maubot";
-  version = "unstable-2022-08-22";
+  version = "unstable-2022-11-17";
 
   src = fetchFromGitHub {
     owner = "maubot";
     repo = pname;
-    rev = "d574f84079c2469b71e6c09c4b804e8bf6156153";
-    sha256 = "sha256-HtViA3i+R05It7ZbSOWc5XpYWfEEJQrdJ8OzPXXW7uU=";
+    rev = "a21b106c71e1bcf61f7e28e1eaeba3f2cb8281d2";
+    sha256 = "sha256-ewi54DtcK2a7JmDKfcbbLs4uQx6W0dZPf60kwvEigqk=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
     aiohttp
     (asyncpg.overridePythonAttrs (old: rec {
       pname = "asyncpg";
-      version = "0.25.0";
+      version = "0.26.0";
 
       src = fetchPypi {
         inherit pname version;
-        sha256 = "sha256-Y/jmppczsoVJfChVRko03mV/LMzSWurutQcYcuk4JUA=";
+        sha256 = "sha256-d+aEok/uF7o+SHypgtAlntF7rhr2gAb0zyhLI7og6iw=";
       };
     }))
     attrs
@@ -77,6 +77,7 @@ python3.pkgs.buildPythonPackage rec {
 
   postPatch = ''
     sed -i -e 's/SQLAlchemy>=1,<1.4/SQLAlchemy/' requirements.txt
+    sed -i -e 's/bcrypt>=3,<4/bcrypt/' requirements.txt
   '';
 
   postInstall = ''
