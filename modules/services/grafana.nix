1bceac8aa57eab483de94c3d802187aa8532e54e
Sumner Evans <me@sumnerevans.com>
2022-11-17 09:20:28 -0700

    grafana: fix on tatooine
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/services/grafana.nix
+++ modules/services/grafana.nix
@@ -1,15 +1,20 @@
 { config, lib, options, pkgs, ... }: with lib; let
   serverName = "grafana.${config.networking.hostName}.${config.networking.domain}";
 in
-mkIf (config.services.grafana.enable && config.hardware.isServer) {
-  services.grafana.domain = serverName;
+mkIf config.services.grafana.enable {
+  services.grafana = {
+    domain = serverName;
+    settings = { };
+  };
 
-  services.nginx.virtualHosts.${config.services.grafana.domain} = {
-    forceSSL = true;
-    enableACME = true;
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
-      proxyWebsockets = true;
+  services.nginx.virtualHosts = mkIf config.hardware.isServer {
+    ${config.services.grafana.domain} = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+      };
     };
   };
 }
