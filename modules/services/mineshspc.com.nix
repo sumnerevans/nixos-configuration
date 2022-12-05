534d439ae5e146734901076b0a81934480df2d7b
sumnerevans <sumnerevans@users.noreply.github.com>
2022-12-05 23:03:43 +0000

    mineshspc.com: update to 87d0112e8a775e9eec9a3467d6483b9cbd98b869

--- modules/services/mineshspc.com.nix
+++ modules/services/mineshspc.com.nix
@@ -25,7 +25,7 @@ in
 
     virtualisation.oci-containers.containers = {
       "mineshspc.com" = {
-        image = "ghcr.io/coloradoschoolofmines/mineshspc.com:e01bc8cb248bc596dd9b3690c3d7b853450ccf11";
+        image = "ghcr.io/coloradoschoolofmines/mineshspc.com:87d0112e8a775e9eec9a3467d6483b9cbd98b869";
         volumes = [ "${cfg.dataDir}:/data" ];
         ports = [ "8090:8090" ];
       };
