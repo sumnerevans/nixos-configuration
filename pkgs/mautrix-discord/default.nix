384d9e90f82d4f4303b7d3d9776a09bcacbe2939
Sumner Evans <me@sumnerevans.com>
2023-01-16 09:32:46 -0700

    mautrix-{discord,slack}: update
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- pkgs/mautrix-discord/default.nix
+++ pkgs/mautrix-discord/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "mautrix-discord";
-  version = "unstable-2022-01-11";
+  version = "unstable-2022-01-16";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "discord";
-    rev = "de59f449f1c68e51d114242c622ddea4f6586d6e";
-    sha256 = "sha256-p9l2MFqvrKIvKBT5hM299qv2hoDrpBMrefFOZqE9rT4=";
+    rev = "315e7921e6c2f82e7c0fa12fea94edb245844e3a";
+    sha256 = "sha256-+GRvMIDAk+3uhDa3jSYHvV5NC+nRYukvhNh5zUYT3Ls=";
   };
 
   buildInputs = [ olm ];
 
-  vendorSha256 = "sha256-VZD+gQv9YsyV7De2A+3/p/LrXJeY2pJKACUy9q8/X3s=";
+  vendorSha256 = "sha256-DVztldaw+UtmJ2tmpsNAYBORWJFs4GuplXiDjJqBsg0=";
 
   meta = with lib; {
     homepage = "https://github.com/mautrix/discord";
--- pkgs/mautrix-slack/default.nix
+++ pkgs/mautrix-slack/default.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "mautrix-slack";
-  version = "unstable-2023-01-10";
+  version = "unstable-2023-01-16";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "slack";
-    rev = "f933a14343ad3bef3059bf64eec02f8a34d81461";
-    sha256 = "sha256-TK6VjQYX2s6hqCP5jJimWFYAjjGCfGRCgtaEDlhcznE=";
+    rev = "6d3f66157904e4a5c1b0ec1452016862d898f18d";
+    sha256 = "sha256-Ysl7u6YRqoEkB+JnutPAtYChQDHruiNT3HB31LlUxqA=";
   };
 
   buildInputs = [ olm ];
 
-  vendorSha256 = "sha256-apGwxV5EugZAJSIbvjmX8cdrcA/c+XEoe/gxB5nhDzU=";
+  vendorSha256 = "sha256-kYaeVXxrfA8WuL10+2DC6c2cYJ2li4/3ulKxcy/KviQ=";
 
   meta = with lib; {
     homepage = "https://github.com/mautrix/slack";
