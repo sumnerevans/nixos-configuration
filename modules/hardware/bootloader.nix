c911888a6c01ec01c4584ea20b7d92a7980dd68d
Sumner Evans <me@sumnerevans.com>
2023-02-23 12:36:10 -0700

    bootloader: limit configurations to not bloat boot partition
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/hardware/bootloader.nix
+++ modules/hardware/bootloader.nix
@@ -4,5 +4,9 @@ in
 {
   boot.cleanTmpDir = true;
   boot.loader.grub.devices = [ "/dev/sda" ];
+
+  boot.loader.grub.configurationLimit = 25;
+  boot.loader.systemd-boot.configurationLimit = 25;
+
   boot.loader.efi.canTouchEfiVariables = true;
 }
