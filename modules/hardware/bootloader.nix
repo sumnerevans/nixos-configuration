f2d29917d5bf24f0eb99386f1598a65430973625
Sumner Evans <me@sumnerevans.com>
2023-06-29 11:04:37 -0600

    /tmp: use tmpfs
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/hardware/bootloader.nix
+++ modules/hardware/bootloader.nix
@@ -2,7 +2,7 @@
   bootloaderCfg = config.boot.loader;
 in
 {
-  # boot.tmp.cleanOnBoot = true;
+  boot.tmp.useTmpfs = true;
   boot.loader.grub.devices = [ "/dev/sda" ];
 
   boot.loader.grub.configurationLimit = 25;
