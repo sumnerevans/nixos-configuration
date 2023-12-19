b21bfc467d30fe12166c650e1dd197847d856afe
Sumner Evans <me@sumnerevans.com>
2023-12-19 12:27:38 -0700

    tailscale: update to use more of the provided config options
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/hardware/networking.nix
+++ modules/hardware/networking.nix
@@ -1,29 +1,19 @@
-{ config, lib, pkgs, ... }: with lib; mkMerge [
-  {
+{ config, pkgs, ... }: {
+  networking = {
     # The global useDHCP flag is deprecated, therefore explicitly set to false
     # here. Per-interface useDHCP will be mandatory in the future, so this
     # generated config replicates the default behaviour.
-    networking.useDHCP = false;
+    useDHCP = false;
 
-    # Enable tailscale across the fleet
-    services.tailscale.enable = true;
-    networking = {
-      firewall.allowedUDPPorts = [ config.services.tailscale.port ];
+    # Only use predictable interface names if using NetworkManager.
+    usePredictableInterfaceNames = config.networking.networkmanager.enable;
+  };
 
-      # NixOS is kinda particular about how you configure the Magic DNS, so we have to do it a bit more manually. See: https://forum.tailscale.com/t/magicdns-nixos/412
-      nameservers = [ "100.100.100.100" ];
-      search = [ "sumnerevans.github.beta.tailscale.net" ];
-    };
-    environment.systemPackages = [ pkgs.tailscale ];
-  }
-
-  (
-    mkIf (!config.networking.networkmanager.enable) {
-      networking.usePredictableInterfaceNames = false;
-
-      services.unbound = {
-        enable = true;
-      };
-    }
-  )
-]
+  # Enable tailscale across the fleet
+  environment.systemPackages = [ pkgs.tailscale ];
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "both";
+    openFirewall = true;
+  };
+}
