5cd5b6f6522c74b7e61711154a7b2b7578709a3c
Sumner Evans <me@sumnerevans.com>
2024-01-09 19:56:43 -0700

    nix: disable remote build
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,12 +1,7 @@
-{ config, lib, pkgs, ... }: with lib; let
-  nixCfg = config.nix;
-in
-{
-  options = {
-    nix.enableRemoteBuildOnCoruscant = mkEnableOption "Enable remote builds on coruscant";
-    nix.enableRemoteBuildOnTatooine = mkEnableOption "Enable remote builds on tatooine";
-  };
-
+{ config, lib, pkgs, ... }:
+with lib;
+let nixCfg = config.nix;
+in {
   config = mkMerge [
     # Allow unfree software.
     {
@@ -20,14 +15,12 @@ in
     }
 
     # If automatic garbage collection is enabled, delete 30 days.
-    (
-      mkIf nixCfg.gc.automatic {
-        nix.gc = {
-          randomizedDelaySec = "45min";
-          options = "--delete-older-than 30d";
-        };
-      }
-    )
+    (mkIf nixCfg.gc.automatic {
+      nix.gc = {
+        randomizedDelaySec = "45min";
+        options = "--delete-older-than 30d";
+      };
+    })
 
     # Use nix flakes
     {
@@ -37,36 +30,6 @@ in
         experimental-features = nix-command flakes
       '';
       nix.package = pkgs.nixUnstable;
-    }
-
-    {
-      programs.ssh =
-        let
-          coruscantPublicIp = lib.removeSuffix "\n" (builtins.readFile ../secrets/coruscant-ip);
-        in
-        {
-          extraConfig = ''
-            Host coruscant
-                IdentityFile /etc/nixos/secrets/nix-remote-build
-                HostName ${coruscantPublicIp}
-                Port 32
-
-            Host coruscant-lan
-                IdentityFile /etc/nixos/secrets/nix-remote-build
-                HostName 192.168.0.14
-                Port 32
-
-            Host tatooine
-                IdentityFile /etc/nixos/secrets/nix-remote-build
-                HostName 5.161.50.43
-          '';
-          knownHosts = {
-            coruscant = {
-              extraHostNames = [ "192.168.0.14" coruscantPublicIp ];
-              publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDcO1lMaMPbL2cr4XdKc6bQJIbQylIXaYfX0S+NN3z0AMw3HCfsNCwlWoxyjIbZBlP3aSrdTITq3eB0gw3l25029h3Q4Dve+I2hf6jpltaGVlpsyhMN8xu9yoqadd0cG71kn6Wn5/BlpaWZtrJy7Px9luCyeuDx+vkC05CLb28sjwYVdTzbuePygUONL7cH6Xd2ulLDW+dFoZIHwraEsqHk9AQRV3f2hokxG/VpbxbVAY7XNOkIrsfmX6y4IccUddffgs8uqsObHEWniPdWOcEocRJ4exORBoyS5SXvcHzUtGi8Q0jGPfKkSFPEYUNcgw0QlU4dzrT/xqm0COcOoXKK58+tZH/YMu0bshp+vIK3HDCCfcRtuv1ZMF/AFbHdY3fglUu3YK2Jpm5Vr8KzljqQXW3ekboILxZpuP2LA3YErS1lpaj3sbOlsfxNQhG7V8/gqo1PBQ4w//7wlav0TOY5GZD1Tw2lduaSAFuFHxVGBOy4Xu31mxa2Qej5YKc71VU= sumner@coruscant-nixos";
-            };
-          };
-        };
     }
   ];
 }
