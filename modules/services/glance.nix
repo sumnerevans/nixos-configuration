7e9d3058e5978cad413af65199075a459c7394de
Sumner Evans <sumner.evans@automattic.com>
2025-02-05 08:37:28 -0700

    feeds: add Chris Kiehl's blog
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- modules/services/glance.nix
+++ modules/services/glance.nix
@@ -99,6 +99,7 @@ in mkIf cfg.enable {
                 { url = "https://blog.babel.sh/rss"; }
                 { url = "https://blog.beeper.com/feed"; }
                 { url = "https://blog.danslimmon.com/feed"; }
+                { url = "https://chriskiehl.com/rss.xml"; }
                 { url = "https://chrismcdonough.substack.com/feed"; }
                 { url = "https://chrpaul.de/feed.xml"; }
                 { url = "https://dominickm.com/feed"; }
