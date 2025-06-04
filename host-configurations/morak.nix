9737b9d3fed1aa18ee1d4ad0b9c365815ce26ced
Sumner Evans <me@sumnerevans.com>
2025-06-03 21:17:39 -0600

    quotesfile: update
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- host-configurations/morak.nix
+++ host-configurations/morak.nix
@@ -3,7 +3,7 @@ let
   quotesfile = pkgs.fetchurl {
     url =
       "https://raw.githubusercontent.com/sumnerevans/home-manager-config/master/modules/email/quotes";
-    hash = "sha256-vWv4XbMcBU9igwcVQLqOylXCH/GAHiTiwDIvr6xWCNs=";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAGAHiTiwDIvr6xWCNs=";
   };
 in {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
