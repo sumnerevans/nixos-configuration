a700b7735f3a1432761e6afebf5e266b9dab8a12
Sumner Evans <me@sumnerevans.com>
2026-01-22 23:52:41 -0700

    tlp: allow charge to 100
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- modules/hardware/laptop.nix
+++ modules/hardware/laptop.nix
@@ -8,8 +8,8 @@
     services.tlp = {
       enable = true;
       settings = {
-        START_CHARGE_THRESH_BAT0 = 75;
-        STOP_CHARGE_THRESH_BAT0 = 80;
+        START_CHARGE_THRESH_BAT0 = 95;
+        STOP_CHARGE_THRESH_BAT0 = 100;
         RESTORE_THRESHOLDS_ON_BAT = 1;
 
         CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
