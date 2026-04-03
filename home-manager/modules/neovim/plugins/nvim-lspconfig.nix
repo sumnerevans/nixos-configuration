0281d091b445966fd0155df05ac8eaf809beb1d5
Sumner Evans <me@sumnerevans.com>
2026-04-02 21:02:26 -0600

    harper: use British
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- home-manager/modules/neovim/plugins/nvim-lspconfig.nix
+++ home-manager/modules/neovim/plugins/nvim-lspconfig.nix
@@ -81,6 +81,7 @@ in
             settings = {
               ["harper-ls"] = {
                 userDictPath = vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
+                dialect = "British",
               }
             },
           })
