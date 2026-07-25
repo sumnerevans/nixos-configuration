a4fd1d486a422b17f8f8bc2b9242d80778f86176
Sumner Evans <me@sumnerevans.com>
2026-07-25 10:50:21 -0600

    tree-sitter: upgrade to latest config format
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- home-manager/modules/neovim/plugins/tree-sitter.nix
+++ home-manager/modules/neovim/plugins/tree-sitter.nix
@@ -10,40 +10,6 @@ in
 {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-      (luaPlugin nvim-treesitter-parsers.bash)
-      (luaPlugin nvim-treesitter-parsers.c)
-      (luaPlugin nvim-treesitter-parsers.comment)
-      (luaPlugin nvim-treesitter-parsers.cpp)
-      (luaPlugin nvim-treesitter-parsers.diff)
-      (luaPlugin nvim-treesitter-parsers.git_config)
-      (luaPlugin nvim-treesitter-parsers.git_rebase)
-      (luaPlugin nvim-treesitter-parsers.gitattributes)
-      (luaPlugin nvim-treesitter-parsers.gitignore)
-      (luaPlugin nvim-treesitter-parsers.go)
-      (luaPlugin nvim-treesitter-parsers.gomod)
-      (luaPlugin nvim-treesitter-parsers.gosum)
-      (luaPlugin nvim-treesitter-parsers.gowork)
-      (luaPlugin nvim-treesitter-parsers.html)
-      (luaPlugin nvim-treesitter-parsers.kotlin)
-      (luaPlugin nvim-treesitter-parsers.javascript)
-      (luaPlugin nvim-treesitter-parsers.javascript)
-      (luaPlugin nvim-treesitter-parsers.json)
-      (luaPlugin nvim-treesitter-parsers.json5)
-      (luaPlugin nvim-treesitter-parsers.lua)
-      (luaPlugin nvim-treesitter-parsers.markdown)
-      (luaPlugin nvim-treesitter-parsers.nix)
-      (luaPlugin nvim-treesitter-parsers.ocaml)
-      (luaPlugin nvim-treesitter-parsers.proto)
-      (luaPlugin nvim-treesitter-parsers.python)
-      (luaPlugin nvim-treesitter-parsers.sql)
-      (luaPlugin nvim-treesitter-parsers.templ)
-      (luaPlugin nvim-treesitter-parsers.terraform)
-      (luaPlugin nvim-treesitter-parsers.tsx)
-      (luaPlugin nvim-treesitter-parsers.vim)
-      (luaPlugin nvim-treesitter-parsers.vimdoc)
-      (luaPlugin nvim-treesitter-parsers.xml)
-      (luaPlugin nvim-treesitter-parsers.yaml)
-
       (luaPlugin vim-matchup)
       {
         type = "lua";
@@ -52,17 +18,23 @@ in
           require'treesitter-context'.setup{mode = 'topline'}
         '';
       }
+
+      (luaPlugin pkgs.vimPlugins.nvim-treesitter.withAllGrammars)
       {
         type = "lua";
         plugin = nvim-treesitter;
         config = ''
-          require('nvim-treesitter').setup({
-            highlight = {
-              enable = true,
-              additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            matchup = { enable = true },
+          require('nvim-treesitter').setup({})
+
+          vim.api.nvim_create_autocmd('FileType', {
+            callback = function(args)
+              local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+              local ok, added = pcall(vim.treesitter.language.add, lang)
+              if lang and ok and added then
+                vim.treesitter.start(args.buf, lang)
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+              end
+            end,
           })
 
           vim.filetype.add({
